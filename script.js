8becce42c50c7d267ec4f47bc7302fb980b6bd1d
Joshua272008 <113116525+Joshua272008@users.noreply.github.com>
2024-05-08 12:54:09 -0400

    fd

---
+++ script.js
@@ -0,0 +1,65 @@
+function negative() {
+    document.getElementById("display").innerHTML+="-";
+}
+function num0() {
+    document.getElementById("display").innerHTML+="0";
+}
+function num1() {
+    document.getElementById("display").innerHTML+="1";
+}
+function num2() {
+    document.getElementById("display").innerHTML+="2";
+}
+function num3() {
+    document.getElementById("display").innerHTML+="3";
+}
+function num4() {
+    document.getElementById("display").innerHTML+="4";
+}
+function num5() {
+    document.getElementById("display").innerHTML+="5";
+}
+function num6() {
+    document.getElementById("display").innerHTML+="6";
+}
+function num7() {
+    document.getElementById("display").innerHTML+="7";
+}
+function num8() {
+    document.getElementById("display").innerHTML+="8";
+}
+function num9() {
+    document.getElementById("display").innerHTML+="9";
+}
+function decimal() {
+    document.getElementById("display").innerHTML+=".";
+}
+function exoponent() {
+    document.getElementById("display").innerHTML+="^";
+}
+function leftParn() {
+    document.getElementById("display").innerHTML+="(";
+}
+function rightParn() {
+    document.getElementById("display").innerHTML+=")";
+}
+function mod() {
+    document.getElementById("display").innerHTML+="%";
+}
+function add() {
+    document.getElementById("display").innerHTML+="+";
+}
+function sub() {
+    document.getElementById("display").innerHTML+="-";
+}
+function mult() {
+    document.getElementById("display").innerHTML+="*";
+}
+function divide() {
+    document.getElementById("display").innerHTML+="/";
+}
+
+function addition()
+{
+
+}
